032f16b2e977bab5423fae584c334e5343fa9a7d
rahulbu <rahulbu83@gmail.com>
2020-02-05 22:25:13 +0530

    added access control for cors

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const express = require('express'),
     methodOverride = require('method-override'),
     knex = require('./db/index'),
     customFunctions = require('./middleware/customFunctions'),
+    cors = require('cors'),
     app = express();
 
 
 
 app.use(helmet())
-
+app.use(cors())
 const tasksRoutes = require('./routes/tasks'),
         indexRoutes = require('./routes/index'),
         userRoutes = require('./routes/user'),
@@ -75,10 +76,10 @@ app.use(function(req,res,next){
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // res.header("Access-Control-Allow-Credentials","true")
 
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8000/');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:8000/');
+    // res.header('Access-Control-Allow-Credentials', true);
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     
 
     next();
